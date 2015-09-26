74ef952e403021788444a3117d735998e567f2f8
Michael Tien <mtien888@gmail.com>
2015-09-26 12:05:02 -0700

    Step 3 finished.  Plus Loading progress bar.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,17 +1,33 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
-import com.udacity.gradle.builditbigger.joke.ManualJoke;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.ymsgsoft.michaeltien.jokeactivity.MyJokeActivity;
+import com.ymsgsoft.udacity.backend.joke.myApi.MyApi;
+
+import java.io.IOException;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainActivityFragment.ProgressBarCallbacks {
+    private ProgressBar mProgressBar;
+
+    @Override
+    public void setProgressBar(ProgressBar progressBar) {
+        mProgressBar = progressBar;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +59,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view){
-
-//        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-        String joke = new ManualJoke().getJoke();
-//        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, MyJokeActivity.class);
-        intent.putExtra(MyJokeActivity.JOKE_KEY, joke);
-        startActivity(intent);
+        mProgressBar.setVisibility(View.VISIBLE);
+        new EndpointsAsyncTask().execute(new Pair<Context, ProgressBar>(this, mProgressBar));
     }
+    public class EndpointsAsyncTask extends AsyncTask<Pair<Context, ProgressBar>, Void, String> {
+        private ProgressBar mProgressBar;
+        private MyApi myApiService = null;
+        private Context context;
+        @Override
+        protected String doInBackground(Pair<Context, ProgressBar>... params) {
+            if (myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+                myApiService = builder.build();
+            }
 
+            context = params[0].first;
+            mProgressBar = params[0].second;
+            // add wait time to test progress
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
 
+            }
+            try {
+                return myApiService.getJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // delay sometime
+
+            mProgressBar.setVisibility(View.GONE);
+            Intent intent = new Intent( context, MyJokeActivity.class);
+            intent.putExtra(MyJokeActivity.JOKE_KEY, result);
+            startActivity(intent);
+        }
+    }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -24,6 +26,10 @@ public class MainActivityFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        ProgressBar progressBar = (ProgressBar) root.findViewById(R.id.LoadingProgressBar);
+        progressBar.setVisibility(View.GONE);
+        if ( progressBar != null)
+            mCallbacks.setProgressBar(progressBar);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
@@ -32,5 +38,30 @@ public class MainActivityFragment extends Fragment {
                 .build();
         mAdView.loadAd(adRequest);
         return root;
+    }
+    private ProgressBarCallbacks mCallbacks = sDummyCallbacks;
+    public interface ProgressBarCallbacks {
+        public void setProgressBar(ProgressBar progressBar);
+    }
+    private static ProgressBarCallbacks sDummyCallbacks = new ProgressBarCallbacks() {
+        @Override
+        public void setProgressBar(ProgressBar progressBar) {
+        }
+    };
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (!(context instanceof ProgressBarCallbacks)) {
+            //throw new IllegalStateException("Activity must implement fragment's callbacks.");
+            return;
+        }
+        mCallbacks = (ProgressBarCallbacks) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = sDummyCallbacks;
     }
 }
